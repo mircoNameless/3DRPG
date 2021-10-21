48531f801c548747860d1810db92672d86fc4489
mircoNameless <lzj2729192447@163.com>
2021-10-21 23:15:05 +0800

    feat:增加人物移动动画，遮挡剔除和树木不检测射线

--- 3D RPG/Assets/Script/Characters/PlayerController.cs
+++ 3D RPG/Assets/Script/Characters/PlayerController.cs
@@ -8,15 +8,27 @@ namespace Script.Characters
     public class PlayerController : MonoBehaviour
     {
         private NavMeshAgent agent;
+        private Animator anim;
 
         private void Awake()
         {
             agent = GetComponent<NavMeshAgent>();
+            anim = GetComponent<Animator>();
         }
 
         private void Start()
         {
             MouseManager.instance.ONMouseClicked += MoveTotTarget;
+        }
+
+        private void Update()
+        {
+            SwitchAnimation();
+        }
+
+        private void SwitchAnimation()
+        {
+            anim.SetFloat("Speed", agent.velocity.sqrMagnitude);
         }
 
         private void MoveTotTarget(Vector3 target)
